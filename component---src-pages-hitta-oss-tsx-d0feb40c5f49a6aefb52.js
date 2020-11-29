126911b430752889a9e03be9ded854d23f830670
Max Bekkhus <maxbekkhus@pop-os.localdomain>
2020-11-30 00:37:07 +0100

    Social media icons

---
+++ component---src-pages-hitta-oss-tsx-d0feb40c5f49a6aefb52.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{JbjL:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),u=t("VXBa"),c=t("uKHj");n.default=function(){return l.a.createElement(u.a,null,l.a.createElement(c.a,null,l.a.createElement("h1",null,"Hitta oss")))}}}]);
+//# sourceMappingURL=component---src-pages-hitta-oss-tsx-d0feb40c5f49a6aefb52.js.map
